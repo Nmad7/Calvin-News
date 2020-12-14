77f9d9554fcf7ef095587c1e087f64141f31ab60
NMad <ngm7@students.calvin.edu>
2020-12-14 12:54:36 -0500

    Attempting with Middleware 6

--- server.js
+++ server.js
@@ -19,9 +19,23 @@ const apiProxy = createProxyMiddleware('/api', options);
 app.use('/api', apiProxy);
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('/latest-news', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/CalvinNews/index.html'));
 });
+
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/CalvinNews/index.html'));
+});
+
+app.get('/search', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/CalvinNews/index.html'));
+});
+
+app.get('/preferences', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/CalvinNews/index.html'));
+});
+
+
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
