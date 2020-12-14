b02dec28f49d4fe2fbaaad07cf572d2e93f5893e
NMad <ngm7@students.calvin.edu>
2020-12-14 11:32:46 -0500

    Second attempt at proxy

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const app = express();
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use('/api', createProxyMiddleware({
-  target: 'https://calvin-cs262-fall2020-teamc.herokuapp.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': ''
-  },
-}));
-
 // Serve static files....
 app.use(express.static(__dirname + '/dist/CalvinNews'));
 
@@ -19,6 +11,15 @@ app.use(express.static(__dirname + '/dist/CalvinNews'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/CalvinNews/index.html'));
 });
+
+app.use('/api', createProxyMiddleware({
+  target: 'https://calvin-cs262-fall2020-teamc.herokuapp.com',
+  secure: true,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': ''
+  },
+}));
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
