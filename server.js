c2eb332fc09532f86263b83b76e09befdffe5041
NMad <ngm7@students.calvin.edu>
2020-12-14 10:10:32 -0500

    Heroku push3

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/Calvin-News'));
+app.use(express.static(__dirname + '/dist/CalvinNews'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Calvin-News/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/CalvinNews/index.html'));
 });
 
 // default Heroku PORT
